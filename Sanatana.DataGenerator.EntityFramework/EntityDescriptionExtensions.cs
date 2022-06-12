503592b5eba68868728fd71ff6465dd5ddbb56fd
RodionKulin <rodion.kulin@mail.ru>
2022-06-12 20:01:48 +0300

    Add EF EntityDescriptionExtensions

---
+++ Sanatana.DataGenerator.EntityFramework/EntityDescriptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Sanatana.DataGenerator.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Sanatana.DataGenerator.Entities
+{
+    public static class EntityDescriptionExtensions
+    {
+        public static EntityDescription<TEntity> AddPersistentStorageEf<TEntity>(
+            this EntityDescription<TEntity> entityDescription, Func<DbContext> dbContextFactory)
+            where TEntity : class
+        {
+            var storage = new EntityFrameworkPersistentStorage(dbContextFactory);
+            entityDescription.AddPersistentStorage(storage);
+            entityDescription.SetRequestCapacityProvider(storage);
+
+            return entityDescription;
+        }
+    }
+}
---
+++ Sanatana.DataGenerator.EntityFrameworkCore/EntityDescriptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sanatana.DataGenerator.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sanatana.DataGenerator.Entities
+{
+    public static class EntityDescriptionExtensions
+    {
+        public static EntityDescription<TEntity> AddPersistentStorageEfCore<TEntity>(
+            this EntityDescription<TEntity> entityDescription, Func<DbContext> dbContextFactory)
+            where TEntity : class
+        {
+            var storage = new EntityFrameworkCorePersistentStorage(dbContextFactory);
+            entityDescription.AddPersistentStorage(storage);
+            entityDescription.SetRequestCapacityProvider(storage);
+
+            return entityDescription;
+        }
+    }
+}
