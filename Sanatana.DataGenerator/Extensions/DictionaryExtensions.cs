e8bfba52f21bca6d581f54132a44d5ce152d0dc7
RodionKulin <rodion.kulin@mail.ru>
2025-03-15 15:52:48 +0700

    Add DictionaryExtensions.

---
+++ Sanatana.DataGenerator/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanatana.DataGenerator.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TEntity GetFirst<TEntity>(this Dictionary<Type, IList> generatedItems)
+        {
+            return (TEntity)generatedItems[typeof(TEntity)][0]!;
+        }
+
+        public static TEntity[] GetArray<TEntity>(this Dictionary<Type, IList> generatedItems)
+        {
+            return generatedItems[typeof(TEntity)].Cast<TEntity>().ToArray();
+        }
+
+        public static List<TEntity> GetList<TEntity>(this Dictionary<Type, IList> generatedItems)
+        {
+            return generatedItems[typeof(TEntity)].Cast<TEntity>().ToList();
+        }
+    }
+}
